9da1a142f672fcd5e1a10d00f86c9f95ce1dab48
Rishav14sep <119671269+Rishav14sep@users.noreply.github.com>
2025-07-23 01:48:59 +0530

    Add files via upload

---
+++ AdvRag/Server.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Query, Path
+from .queue.connection import queue
+from .queue.worker import process_query
+
+app = FastAPI()
+
+
+@app.get('/')
+def root():
+    return {"status": 'Server is up and running'}
+
+@app.post('/chat')
+def chat(
+    query: str = Query(..., description="Chat Message")
+):
+    # Query ko Queue mei daal do
+    job = queue.enqueue(process_query, query)  # process_query(query)
+
+    # User ko bolo your job received
+    return {"status": "queued", "job_id": job.id}
